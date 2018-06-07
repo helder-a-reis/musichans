90d82787495725aeebcd2b31d17e0ab6b0804035
helder-a-reis <helderreis78@gmail.com>
2018-06-07 21:59:47 +0200

    Created Musician model extending User

--- musichans/members/admin.py
+++ musichans/members/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from members.models import Musician
 
 # Register your models here.
+'''Define an inline admin descriptor for Employee model which acts a bit like a singleton
+https://docs.djangoproject.com/en/1.8/topics/auth/customizing/#extending-the-existing-user-model '''
+
+class MusicianInline(admin.StackedInline):
+    model = Musician
+    can_delete = False
+    verbose_name_plural = 'musician'
+    
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (MusicianInline, )
+    
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)    
+
--- musichans/members/models.py
+++ musichans/members/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Musician(models.Model):
+    
+    #We extend Django’s built-in authentication system that already has things like 
+    #userName, email, password, etc, and just extend it with the Musician's profile fields
+    #https://docs.djangoproject.com/en/1.8/topics/auth/customizing/#extending-the-existing-user-model
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    country = models.CharField(max_length=128)
+    city = models.CharField(max_length=128)
+
+    
+    
+        
